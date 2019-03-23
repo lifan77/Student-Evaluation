e322afebed9a1129dea037a2759fcb835a1afd6c
DESKTOP-3HLI1K2\DELL <940241312@qq.con>
2019-03-23 16:40:24 +0800

    build

--- evaluation/tangcco-evaluation-biz/src/main/java/com/tangcco/evaluation/service/impl/ClassServiceImpl.java
+++ evaluation/tangcco-evaluation-biz/src/main/java/com/tangcco/evaluation/service/impl/ClassServiceImpl.java
@@ -2,6 +2,7 @@ package com.tangcco.evaluation.service.impl;
 
 import com.tangcco.evaluation.dao.ClassMapper;
 import com.tangcco.evaluation.service.ClassService;
+import com.tangcoo.evaluation.dto.PageDto;
 import com.tangcoo.evaluation.pojo.Class;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,4 +25,26 @@ public class ClassServiceImpl implements ClassService {
     public List<Class> select() {
         return classMapper.selectAll();
     }
+
+    @Override
+    public Integer getClassCount(String className, String gradeName) {
+        return classMapper.getClassCount(className,gradeName);
+    }
+
+    @Override
+    public PageDto<Class> selectClassByClassGrade(Integer pageNo, Integer pageSize, String className, String gradeName) {
+        int offset=(pageNo-1)*pageSize;
+        List<Class> classList = classMapper.selectClassByClassGrade(offset,pageSize,className,gradeName);
+        System.out.println(classList);
+        Long classCount = (long)classMapper.getClassCount(className,gradeName);
+        PageDto<Class> classPageDto =new PageDto<>();
+        classPageDto.setTotal(classCount);
+        classPageDto.setPageSize(pageSize);
+        classPageDto.setPageNo(pageNo);
+        classPageDto.setData(classList);
+        int pages=(int)(classCount/pageSize+(classCount%pageSize>0?1:0));
+        classPageDto.setPages(pages);
+        return classPageDto;
+    }
+
 }
